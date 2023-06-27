7e36f92df3bc4ce7f067970b53abee883d2c6bc7
Jaewoo <jaewoocho1017@gmail.com>
2023-06-26 18:42:21 -0700

    updated

--- pages/api/sendEmail.tsx
+++ pages/api/sendEmail.tsx
@@ -30,7 +30,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 			await transporter.sendMail(mailOptions);
 			res.status(200).json({ message: "Email sent successfully" });
 		} catch (error) {
-			// console.error(error);
+			console.error(error);
 			res.status(500).json({ error: "error sending email" });
 		}
 	}
