dfd86e585bd508a722253ea26d04638cfb2e6161
Jaewoo <jaewoocho1017@gmail.com>
2023-08-04 11:24:33 -0700

    updated

--- pages/api/graphql.ts
+++
@@ -1,9 +0,0 @@
-
-const {ApolloServer} = require("apollo-server");
-
-const server = new ApolloServer({typeDefs, resolvers});
-
-
-server.listen().then(() => {
-    console.log("YOUR API IS RUNNING :");
-})
